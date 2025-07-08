0aa80725ea83374f423789e8c26ea6921c496433
FeliCres7 <147621499+FeliCres7@users.noreply.github.com>
2025-07-08 15:08:48 -0300

    Update api.py

--- api.py
+++ api.py
@@ -52,4 +52,4 @@ async def predict(data: InputData):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("api:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
